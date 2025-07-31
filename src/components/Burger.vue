64ffa4a64ed55326d65b3f49a653087668f6a76a
Admin <admin@Admins-MacBook-Pro.local>
2025-07-31 17:04:06 +0100

    IA and burger

---
+++ src/components/Burger.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+
+function toggle() {
+  isOpen.value = !isOpen.value
+}
+
+console.log("burger")
+
+</script>
+
+<template>
+
+  <a
+    class="nav__button nav__button--burger"
+    href="#"
+    @click.prevent="toggle"
+  >
+    <i class="fa-solid fa-burger"></i>
+  </a>
+
+  <Teleport to="#js-overlay">
+    <nav v-if="isOpen" class="overlay" @click.self="toggle">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  </Teleport>
+</template>
+
+
